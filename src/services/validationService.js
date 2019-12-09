e1c820c872c814d357b2941ae301c14e0e27c4c8
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-09 17:38:47 -0600

    Added a new service class for validation

---
+++ src/services/validationService.js
@@ -0,0 +1,8 @@
+export default class ValidationService {
+    static validateEmail(email) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return true;
+        }
+        return false;
+    }
+}
