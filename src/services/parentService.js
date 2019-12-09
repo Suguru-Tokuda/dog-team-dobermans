362b64a92af5e7a24215fa60a6e185f0c24e919c
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-09 17:33:14 -0600

    Added functions for parent

--- src/services/parentService.js
+++ src/services/parentService.js
@@ -1,9 +1,13 @@
+import SessionInfoService from './sessionInfoService';
+import * as api from '../api.json';
+import axios from 'axios';
+
 export default class ParentService {
     static getAllParents() {
-        return `${SessionInfoService.getBaseUrlForAPI()}parents`;
+        return axios.get(`${SessionInfoService.getBaseUrlForAPI()}parents?key=${api.API_KEY}`);
     }
 
     static getParent(parentID) {
-
+        return axios.get(`${SessionInfoService.getBaseUrlForAPI()}parent?key=${api.API_KEY}&parentID=${parentID}`);
     }
 }
