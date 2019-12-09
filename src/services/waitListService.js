f23696a88acbea2875bef897ebbaa5e8171fa69f
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-09 17:46:32 -0600

    Added a new service class for waitlist

---
+++ src/services/waitListService.js
@@ -0,0 +1,22 @@
+import SessionInfoService from './sessionInfoService';
+import * as api from '../api.json';
+import axios from 'axios';
+
+export default class WaitListService {
+    static getServiceBase() {
+        return `${SessionInfoService.getBaseUrlForAPI()}waitList`;
+    }
+
+    static createWaitRequest(firstName, lastName, email, phone, message, color, date) {
+        const data = {
+            firsName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            message: message,
+            color: color,
+            create: date
+        };
+        return axios.post(`${this.getServiceBase()}?key=${api.API_KEY}`, data);
+    }
+}
