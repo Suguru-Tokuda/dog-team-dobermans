c335e290f52314fe2e31dfbab8021cb7a9cf992e
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-09 17:31:56 -0600

    Inserted video

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Topnavbar from '../common/topnavbar';
-// import '../../assets/css/style.violet.css';
 
 class Main extends Component {
     render() {
         return (
-            <div style={{paddingTop: '100px'}}>
-                <Topnavbar />
-                <h1 className="text-uppercase">Main</h1>
+            <div>
+                <section className="hero-video">
+                    <video muted autoPlay loop src="https://firebasestorage.googleapis.com/v0/b/dogteamdobermans.appspot.com/o/mainVideo%2FolCKecjEqf?alt=media&token=9f707307-3126-44d9-b32b-d711ff94ae63" className="bg-video"></video>
+                </section>
             </div>
         );
     }
