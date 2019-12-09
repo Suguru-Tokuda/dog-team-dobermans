5768a7d7b5b96ab94d214eb848008b18e92579c5
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-09 17:33:52 -0600

    Added functions for puppies

--- src/services/puppyService.js
+++ src/services/puppyService.js
@@ -1,12 +1,18 @@
 import SessionInfoService from './sessionInfoService';
+import * as api from '../api.json';
+import axios from 'axios';
 
 export default class PuppyService {
 
     static getServiceBase() {
-        return `${SessionInfoService.getBaseUrlForAPI()}puppies`
+        return `${SessionInfoService.getBaseUrlForAPI()}`
     }
 
     static getAllPuppies() {
+        return axios.get(`${this.getServiceBase()}puppies?key=${api.API_KEY}`)
+    }
 
+    static getPuppy(puppyID) {
+        return axios.get(`${this.getServiceBase()}puppy?key=${api.API_KEY}&puppyID=${puppyID}`);
     }
 }
