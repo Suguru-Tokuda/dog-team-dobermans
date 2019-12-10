f1e38575396735006586b3de85692add3e6d3039
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-10 09:41:16 -0600

    Added Testimonials component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './components/main/main';
 import TopNavbar from './components/common/topnavbar';
 import Footer from './components/common/footer';
-import Puppies from './components/puppies/puppies';
 import Parents from './components/parents/parents';
+import Puppies from './components/puppies/puppies';
+import Testimonials from './components/testimonials/testimonials';
 import AboutUs from './components/aboutUs/aboutUs';
 import Contact from './components/contact/contact';
 import PageNotFound from './components/common/pageNotFound';
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/" exact render={(props) => <Main {...props} />} />
             <Route path="/puppies" render={(props) => <Puppies {...props} />} />
             <Route path="/our-dogs" render={(props) => <Parents {...props} />} />
+            <Route path="/testimonials" render={(props) => <Testimonials {...props} />} />
             <Route path="/about-us" render={(props) => <AboutUs {...props} />} />
             <Route path="/contact" render={(props) => <Contact {...props} />} />
             <Route component={PageNotFound} />
