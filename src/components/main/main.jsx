2de116556cf0f0aa3fa78906477f658a46cfd950
Suguru Tokuda <sugurut@wolfram.com>
2019-12-23 12:46:17 -0600

    Added playsinline for video

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -41,7 +41,7 @@ class Main extends Component {
             <div>
                 <section className="hero-video">
                     {/* <video muted autoPlay loop src="https://firebasestorage.googleapis.com/v0/b/dogteamdobermans.appspot.com/o/mainVideo%2FolCKecjEqf?alt=media&token=9f707307-3126-44d9-b32b-d711ff94ae63" className="bg-video"></video> */}
-                    <video muted autoPlay loop src={videoSrc} className="bg-video"></video>
+                    <video muted autoPlay loop playsInline src={videoSrc} className="bg-video"></video>
                     <div className="container position-relative text-white text-center">
                         <div className="row">
                             <div className="col-xl-7 mx-auto">
