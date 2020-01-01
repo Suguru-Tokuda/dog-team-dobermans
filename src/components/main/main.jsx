3d78746fa648a0834575cb41997878da3444f074
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-12-31 21:48:29 -0600

    Added our dogs and puppies in the main page

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import HomepageContentsService from '../../services/homepageContentsService';
+import PuppyService from '../../services/puppyService';
+import ParentService from '../../services/parentService';
 import toastr from 'toastr';
+import $ from 'jquery';
 
 class Main extends Component {
     state = {
         videoSrc: '',
-        news: ''
+        news: '',
+        puppies: [],
+        parents: []
     };
 
     componentDidMount() {
+        PuppyService.getLivePuppiesForLimit(10)
+            .then(res => {
+                this.setState({ puppies: res.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        ParentService.getLiveParentsForLimit(3)
+            .then(res => {
+                this.setState({ parents: res.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
         HomepageContentsService.getHomepageContents()
             .then(async (res) => {
                 const homepageContentsData = res.data;
                 if (typeof homepageContentsData.backgroundVideo !== 'undefined') {
                     this.setState({ videoSrc: homepageContentsData.backgroundVideo.url })
-                    // await fetch(homepageContentsData.backgroundVideo.url)
-                    //     .then(res => res.blob())
-                    //     .then(blob => {
-                    //         const objectURL = URL.createObjectURL(blob);
-                    //         this.setState({ videoSrc: objectURL });
-                    //     })
-                    //     .catch(err => {
-                    //         console.log(err);
-                    //         toastr.error('There was an error in loading the home page data');
-                    //     });
                 }
                 if (typeof homepageContentsData.news !== 'undefined') {
                     this.setState({ news: homepageContentsData.news });
@@ -34,6 +44,89 @@ class Main extends Component {
                 toastr.error('There was an error in loading the home page data');
             });
     };
+
+    componentDidUpdate() {
+        $(document).ready(function () {
+            $('.owl-carousel').owlCarousel({
+                items: 4
+            });
+        });
+    }
+
+    getOurDogs() {
+        const { parents } = this.state;
+        if (parents.length > 0) {
+            const items = parents.map(parent => {
+                let imageUrl = '';
+                if (typeof parent.pictures !== 'undefined' && parent.pictures.length > 0) {
+                    imageUrl = parent.pictures[0].url;
+                }
+                return (
+                    <div className="col-lg-4" key={`our-dog-${parent.parentID}`}>
+                        <Link to={`/our-dogs/${parent.parentID}`}>
+                            <div style={{backgroundImage: `url(${imageUrl})`}} className="item d-flex align-items-end">
+                                <div className="content">
+                                    <h3 className="h5">{parent.name}</h3>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                );
+            });
+            return (
+                <section className="categories">
+                    <div className="container">
+                        <header className="text-center">
+                            <h2 className="text-uppercase">Our Dogs</h2>
+                        </header>
+                        <div className="row text-left">
+                            {items}
+                        </div>
+                    </div>
+                </section>
+            )
+        }
+        return null;
+    }
+
+    getPuppies() {
+        const { puppies } = this.state;
+        if (puppies.length > 0) {
+            const items = puppies.map(puppy => {
+                return (
+                    <div key={`puppy-${puppy.puppyID}`} className="item">
+                        <div className="product is-gray">
+                            <div className="image d-flex align-items-center justify-content-center">
+                                <img src={puppy.pictures[0].url} alt={puppy.pictures[0].reference} className="img-fluid" />
+                                <div className="hover-overlay d-flex align-items-center justify-content-center">
+                                    {puppy.sold === true && (
+                                        <div className="ribbon ribbon-danger text-uppercase">Sold</div>
+                                    )}
+                                    <div className="CTA d-flex align-items-center justify-content-center">
+                                        <Link to={`/puppies/${puppy.puppyID}`} className="visit-product active"><i className="fa fa-search"></i>View</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            });
+            return (
+                <section className="men-collection gray-bg">
+                    <div className="container">
+                        <header className="text-center">
+                            <h2 className="text-uppercase">
+                                Pupppies
+                            </h2>
+                        </header>
+                        <div data-slider-id="1" className="owl-carousel">
+                            {items}
+                        </div>
+                    </div>
+                </section>
+            );
+        }
+    }
 
     render() {
         const { videoSrc, news } = this.state;
@@ -46,15 +139,20 @@ class Main extends Component {
                         <div className="row">
                             <div className="col-xl-7 mx-auto">
                                 <h1 className="text-uppercase text-shadow letter-spacing-2 mb-4">Dog Team Dobermans</h1>
-                                <hr class="bg-light m-5"></hr>
-                                <p class="lead mb-5">We breed quality dobermans.</p>
+                                <hr className="bg-light m-5"></hr>
+                                <p className="lead mb-5">We breed quality dobermans.</p>
                             </div>
                         </div>
                     </div>
                 </section>
+                {this.getOurDogs()}
+                {this.getPuppies()}
                 {news !== '' && (
-                    <section className="blog gray-bg">
+                    <section className="blog">
                         <div className="container">
+                        <header className="text-center">
+                            <h2 className="text-uppercase">News</h2>
+                        </header>
                             <div dangerouslySetInnerHTML={{ __html: news }} />
                         </div>
                     </section>
--- src/services/parentService.js
+++ src/services/parentService.js
@@ -3,8 +3,21 @@ import * as api from '../api.json';
 import axios from 'axios';
 
 export default class ParentService {
+
+    static getServiceBase() {
+        return `${SessionInfoService.getBaseUrlForAPI()}parents?key=${api.API_KEY}`;
+    }
+
     static getAllParents() {
-        return axios.get(`${SessionInfoService.getBaseUrlForAPI()}parents?key=${api.API_KEY}`);
+        return axios.get(this.getServiceBase());
+    }
+
+    static getAllLiveParents() {
+        return axios.get(`${this.getServiceBase()}&live=true`);
+    }
+
+    static getLiveParentsForLimit(limit) {
+        return axios.get(`${this.getServiceBase()}&live=true&limit=${limit}`);
     }
 
     static getParent(parentID) {
