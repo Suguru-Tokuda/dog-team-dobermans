4a139b1eac13e1b55337cba2b5cefe3c6b0b6096
Suguru Tokuda <suguru.tokuda@gmail.com>
2020-12-26 00:05:42 -0600

    Modified the details

--- src/components/puppyRequests/puppyRequestDetail.jsx
+++ src/components/puppyRequests/puppyRequestDetail.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Messages from './messages';
 import Messenger from './messenger';
+import waitListService from '../../services/waitListService';
+import toastr from 'toastr';
+import WaitListService from '../../services/waitListService';
 
 class PuppyRequestDetail extends Component {
     state = {
-        requestID: 0,
+        userID: '',
+        requestID: '',
         messages: []
     };
 
@@ -17,14 +22,53 @@ class PuppyRequestDetail extends Component {
             this.state.requestFound = false;
         else
             this.state.requestID = reuqestID;
+
+        if (this.props.user) {
+            this.state.userID = this.props.user.userID;
+        }
     }
 
-    componentDidMount() {
-        const { requestID } = this.state;
+    componentDidMount = async () => {
+        const { requestID, userID } = this.state;
 
         window.scrollTo(0, 0);
 
-        // TODO API call to get the request details with messages.
+        this.props.showLoading({ reset: false, count: 1 });
+
+        try { 
+            const messagesRes = waitListService.getWaitRequestMessages(requestID);
+            const messages = messagesRes.data;
+
+            if (messages.length > 0) {
+                const messageIDsMarkAsRead = [];
+
+                messages.forEach(message => {
+                    if (message.recipientID === this.props.user.userID)
+                        messageIDsMarkAsRead.push(message.messageID);
+                });
+
+                if (messageIDsMarkAsRead.length > 0) {
+                    await WaitListService.markMessageAsRead(messageIDsMarkAsRead);
+                }
+            }
+        } catch (err) {
+            console.log(err);
+            toastr.error('There was an error in loading messages.');
+        }
+
+        waitListService.getWaitRequestMessages(requestID)
+            .then(res => {
+                this.setState({
+                    messages: res.data
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                toastr.error('There was an error in loading messages.');
+            })
+            .finally(() => {
+                this.props.doneLoading({ reset: true });
+            });
     }
 
     getHeader() {
@@ -52,17 +96,43 @@ class PuppyRequestDetail extends Component {
         );
     }
 
+    handleUpdateMessages = (messages) => {
+        if (messages.length > 0)
+            this.setState({ messages: messages });
+    }
+
     render() {
         return (
             <React.Fragment>
                 {this.getHeader()}
                 <div className="mb-3">
-                    <Messenger />
+                    <Messenger {...this.props}
+                               messages={this.state.messages}
+                               requestID={this.state.requestID}
+                               updateMessages={this.handleUpdateMessages.bind(this)}
+                    />
                 </div>
-                <Messages />
+                <Messages {...this.props} 
+                          messages={this.state.messages} 
+                />
             </React.Fragment>
         );
     }
 }
 
-export default PuppyRequestDetail;
+const mapStateToProps = state => ({
+    user: state.user,
+    authenticated: state.authenticated
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: () => dispatch({ type: 'SIGN_IN' }),
+        setUser: (user) => dispatch({ type: 'SET_USER', user: user }),
+        showLoading: (params) => dispatch({ type: 'SHOW_LOADING', params: params }),
+        doneLoading: () => dispatch({ type: 'DONE_LOADING' }),
+        setRedirectURL: (url) => dispatch({ type: 'SET_REDIRECT_URL', url: url })
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PuppyRequestDetail);
