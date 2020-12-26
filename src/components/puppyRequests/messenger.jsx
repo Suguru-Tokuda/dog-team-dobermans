604a1765ef3f88e69ce2833c0f62ba086deafa6e
Suguru Tokuda <suguru.tokuda@gmail.com>
2020-12-25 23:55:35 -0600

    Added logic to send message

--- src/components/puppyRequests/messenger.jsx
+++ src/components/puppyRequests/messenger.jsx
@@ -1,17 +1,66 @@
-import { ReadStream } from 'fs-extra';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import waitListService from '../../services/waitListService';
+import toastr from 'toastr';
 
 class Messenger extends Component {
     state = {
-        requestID: 0,
-        messageBody: ''
+        userID: '',
+        requestID: '',
+        messageBody: '',
+        messages: []
     };
 
     constructor(props) {
         super(props);
         
-        // if (props.requestID)
-        //     this.state.requestID;
+        if (this.props.user) {
+            this.state.userID = this.props.user.userID;
+        }
+        
+        if (this.props.requestID) {
+            this.state.requestID = this.props.requestID;
+        }
+    }
+
+    componentDidUpdate() {
+        const { messages } = this.state;
+
+        this.props.updateMessages([]);
+
+        if (this.props.messages && JSON.stringify(messages) !== JSON.stringify(this.props.messages)) {
+            this.setState({ messages: this.props.messages });
+        }
+    }
+
+    handleSendBtnClicked = () => {
+        const { userID, requestID, messageBody } = this.state;
+
+        if (messageBody.length > 0) {
+            this.props.showLoading({ reset: true, count: 1 });
+
+            
+            waitListService.sendWaitRequestMessage(userID, requestID, messageBody)
+                .then(res => {
+                    let { messages } = this.state;
+
+                    messages.push(res.data);
+                    messages = messages.sort((messageA, messageB) => {
+                        return messageA.sentDate > messageB.sentDate ? -1 : messageA.sentDate < messageB.sentDate ? 1 : 0;
+                    });
+
+                    this.props.updateMessages(messages);
+
+                    toastr.success('A message was successfully sent.');
+                })
+                .catch(err => {
+                    console.log(err);
+                    toastr.error('There was an error in sending a message.');
+                })
+                .finally(() => {
+                    this.props.doneLoading({ reset: true });
+                });
+        }
     }
 
     renderTextEditor = () => {
@@ -25,16 +74,16 @@ class Messenger extends Component {
                     </div>
                 </div>
                 <div className="form-grup row">
-                    <div class="col-12">
+                    <div className="col-12">
                         <div style={{ padding: '10px'}}>
                             <textarea className="form-control" rows="10" style={{ resize: 'none' }} value={ messageBody } onChange={ e => this.setState({ messageBody: e.target.value }) }></textarea>
                         </div>
                     </div>
                 </div>
                 <div className="form-group row">
-                    <div class="col-12">
-                        <div class="pull-right">
-                            <button className="btn btn-primary"><i className="fa fa-send"></i> Send</button>
+                    <div className="col-12">
+                        <div className="pull-right">
+                            <button className="btn btn-primary" onClick={this.handleSendBtnClicked}><i className="fa fa-send"></i> Send</button>
                         </div>
                     </div>
                 </div>
@@ -51,4 +100,19 @@ class Messenger extends Component {
     }
 }
 
-export default Messenger;
+const mapStateToProps = state => ({
+    user: state.user,
+    authenticated: state.authenticated
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: () => dispatch({ type: 'SIGN_IN' }),
+        setUser: (user) => dispatch({ type: 'SET_USER', user: user }),
+        showLoading: (params) => dispatch({ type: 'SHOW_LOADING', params: params }),
+        doneLoading: () => dispatch({ type: 'DONE_LOADING' }),
+        setRedirectURL: (url) => dispatch({ type: 'SET_REDIRECT_URL', url: url })
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Messenger);
