a3723aaf0c17f737d1aa2b7026c402575bf7964d
Suguru Tokuda <suguru.tokuda@gmail.com>
2020-12-25 23:54:43 -0600

    Added index for puppy requests

--- src/components/puppyRequests/index.jsx
+++ src/components/puppyRequests/index.jsx
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import PuppyRequestList from './puppyRequestList';
 import PuppyRequestDetail from './puppyRequestDetail';
+import { connect } from 'react-redux';
 
 export class PuppyRequests extends Component {
-
     constructor(props) { 
         super(props);
     }
 
+    componentDidMount() {
+        if (this.props.authenticated === false) {
+            this.props.setRedirectURL(this.props.location.pathname);
+        }
+    }
+
     render() {
-        return (
-            <React.Fragment>
-                <Route path="/puppy-requests" exact render={(props) => <PuppyRequestList name="Puppy Requests" {...props} />} />
-                <Route path="/puppy-requests/:puppyRequestID" exact render={(props) => <PuppyRequestDetail {...props} />} />
-            </React.Fragment>
-        );
+        const { authenticated, user } = this.props;
+
+        if (authenticated === true && user.emailVerified) {
+            return (
+                <React.Fragment>
+                    <Route path="/puppy-requests" exact render={(props) => <PuppyRequestList {...props} />} />
+                    <Route path="/puppy-requests/:puppyRequestID" exact render={(props) => <PuppyRequestDetail {...props} />} />
+                </React.Fragment>
+            );
+        } else if (!authenticated) {
+            this.props.setRedirectURL(this.props.location.pathname);
+            return <Redirect to={{ pathname: "/login", state: { previousUrl: this.props.location.pathname } }}/>;
+        } else if (authenticated && !user.emailVerified) {
+            return <Redirect to="/email-verifiation" />;
+        }
     }
 }
+
+const mapStateToProps = state => ({
+    user: state.user,
+    authenticated: state.authenticated
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: () => dispatch({ type: 'SIGN_IN' }),
+        setUser: (user) => dispatch({ type: 'SET_USER', user: user }),
+        showLoading: (params) => dispatch({ type: 'SHOW_LOADING', params: params }),
+        doneLoading: () => dispatch({ type: 'DONE_LOADING' }),
+        setRedirectURL: (url) => dispatch({ type: 'SET_REDIRECT_URL', url: url })
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PuppyRequests);
