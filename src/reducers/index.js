e23550ba4fb4661d7ca5ff1cda5860a7f331a19f
Suguru Tokuda <suguru.tokuda@gmail.com>
2020-12-25 23:35:11 -0600

    Added a reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,15 @@ import authenticationReducer from './authentiation';
 import userReducer from './user';
 import loader from './loader';
 import userChecked from './userStatusChecked';
+import redirectURL from './redirect';
 import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
     authenticated: authenticationReducer,
     user: userReducer,
     loadCount: loader,
-    userChecked: userChecked
+    userChecked: userChecked,
+    redirectURL: redirectURL
 });
 
 export default allReducers;
